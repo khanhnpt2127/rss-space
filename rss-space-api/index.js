8b935513462ae8dd795d8cf616798b95a2e83746
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-05-28 15:56:22 +0200

    add middleware

--- rss-space-api/index.js
+++ rss-space-api/index.js
@@ -15,6 +15,13 @@ let bodyParser = require('body-parser');
 let config = require('config'); 
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 app.get('/', (req, res) => res.json({message: 'Wellcome to RSS-Space-API'}))
 
 app.use('/api', router);
