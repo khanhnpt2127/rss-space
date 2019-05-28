46df76994ab7355ac903802ec66f6bc0c9494eac
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-05-28 15:04:35 +0200

    fix error

--- rss-space-api/index.js
+++ rss-space-api/index.js
@@ -1,7 +1,23 @@
+// Express 
 const express = require('express')
 const app = express()
 const port = 3000
 
+//  Mongoosse 
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+
+// Route
+//let book = require('./app/routes/book');
+
+// Config
+let config = require('config'); 
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+module.exports = app; 
