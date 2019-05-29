5bb87572dd9b90b9f1bfe386837d3e61dbf93e75
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-05-29 23:18:04 +0200

    rss parser

--- rss-space-feedServices/index.js
+++ rss-space-feedServices/index.js
@@ -1,5 +1,32 @@
+// Schedule JOB
 var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('*/1 * * * *', function(){
+
+// RSS Parser
+let Parser = require('rss-parser');
+/*
+var j = schedule.scheduleJob('*x/1 * * * *', function(){
 	console.log('1minute yay' );
 });
+*/
+let parser = new Parser({
+ customFields: {
+    feed: ['updated'],
+    item: ['author:name','updated'],
+  }
+});
+
+(async () => {
+ 
+  let feed = await parser.parseURL('https://www.reddit.com/r/news/.rss');
+  console.log(feed.title);
+	console.log(feed.updated);
+	console.log("\n");
+  feed.items.forEach(item => {
+    console.log(item.title + "\n" + item.link + "\n" + item.updated + "\n" +  item.author);
+		console.log("\n");
+  });
+ 
+})();
+
+
+
