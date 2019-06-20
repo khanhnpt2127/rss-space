6ac1fde23162ee56a2b557c68e1392302ac1dbbc
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-06-20 06:50:45 +0200

    push all

---
+++ rss-space-api-remake/src/controllers/userController.js
@@ -0,0 +1,56 @@
+// Import MODEL 
+let userSchema = require('../models/user')
+
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
+
+
+
+
+exports.getAll = function(req,res) {
+
+    userSchema.find({}, function(err, user){
+        res.status(200).send(user);
+    })
+
+}
+
+exports.create = function(req, res) {
+
+   
+    userSchema.find({email: req.body.email}, (err, result) => {
+        if(err) console.log(err)
+        if(result.length == 0) {
+            var user = new userSchema(req.body)
+
+            user.save((err, user) => {
+                if(err) console.log(err)
+                res.status(201).send(user)       
+            }) 
+            return;
+        }
+        res.status(412).send({ "msg": "duplicated" })
+    })
+
+}
+
+exports.authenticate = function(req,res) {
+    userSchema.find({email: req.body.email}, (err, userInfo) => {
+        if(err) console.log(err)
+
+        if (userInfo.length == 0)  {
+            res.status(412).send({ "msg": "user is not existed" }) 
+            return;
+        }
+
+        bcrypt.compare(req.body.password, userInfo[0].password).then((match) => {
+            if(match) {
+                res.status(200).send({"msg" : "OK"})
+            } else {
+                res.send({"msg": "fail"}) 
+            }   
+        })
+
+        })
+}
+
---
+++ rss-space-api-remake/src/routes/user.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var router = express.Router()
+
+// Controller 
+var userController = require('../controllers/userController');
+
+
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    // LOG 
+//    console.log('Has been pinged to FEED endpoint! Time: ', Date.now())
+    next()
+  })
+
+  
+
+router.route('/')
+  .get(userController.authenticate)
+  .post(userController.create)
+
+/*
+router.route('/:id')
+  .get(userController.getById)
+*/
+module.exports = router;
